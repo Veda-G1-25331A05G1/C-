d68339ee1d3ce988f922a9d48565a7dd46d6af10
Veda sai <25331a05g1@mvgrce.edu.in>
2026-09-24 15:36:23 +0530

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Different data types
+    int age = 20;
+    float height = 5.8f;
+    double salary = 25000.50;
+    char grade = 'A';
+    bool passed = true;
+    string name = "Veda";
+
+    // Display the values
+    cout << "Name   : " << name << endl;
+    cout << "Age    : " << age << endl;
+    cout << "Height : " << height << endl;
+    cout << "Salary : " << salary << endl;
+    cout << "Grade  : " << grade << endl;
+    cout << "Passed : " << passed << endl;
+
+    return 0;
+}
